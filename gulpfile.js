96b6ce443212d6e345ab7f5191677aacf51dd317
Majid Ataee <ataiemajid_63@hotmail.com>
2018-11-11 19:10:01 +0330

    Updated gulp file.

---
+++ gulpfile.js
@@ -0,0 +1,46 @@
+const gulp = require('gulp');
+const concat = require('gulp-concat');
+const minify = require('gulp-minify');
+const rename = require('gulp-rename');
+const sass = require('gulp-sass');
+const replace = require('gulp-replace');
+ 
+const regex = /^(import|export).*;{1}$/gm;
+
+gulp.task('js-build', function(){
+  return gulp.src(['./src/*.js', ], {base:'./'})
+    .pipe(concat('date-picker.js'))
+    .pipe(replace(regex, ''))
+    .pipe(gulp.dest('dist'))
+});
+
+gulp.task('js-minify', function(){
+  return gulp.src(['./src/*.js', ], {base:'./'})
+    .pipe(concat('date-picker.js'))
+    .pipe(replace(regex, ''))
+    .pipe(minify())
+    .pipe(gulp.dest('dist'))
+    .pipe(rename('calendar-view.min.js'))
+});
+
+gulp.task('sass-build', function () {
+  return gulp.src('./src/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('sass-minify', function () {
+  return gulp.src('./src/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('default', ['sass-minify', 'js-minify']);
+
+gulp.task('minify', ['sass-minify', 'js-minify']);
+
+gulp.task('build', ['sass-build', 'js-build']);
+ 
+gulp.task('watch', function () {
+  gulp.watch('./src', ['sass-build','js-build']);
+});
